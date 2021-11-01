25d4332464604f97a9d0db3d72308934ce9f7b6a
tprat <Titouan.p@hotmail.fr>
2021-11-01 22:59:35 +0100

    db init

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+City.destroy_all
+Gossip.destroy_all
+Tag.destroy_all
+JoinTableGossipTag.destroy_all
+
+15.times do
+	City.create(
+		name: Faker::Address.city,
+		zip_code: Faker::Address.zip_code)
+end
+
+10.times do
+	User.create(
+		first_name: Faker::Name.first_name,
+		last_name: Faker::Name.last_name,
+		description: Faker::GreekPhilosophers.quote,
+		email: Faker::Internet.email,
+		age: rand(18..100),
+		city: City.all.sample)
+end
+
+5.times do
+	Tag.create(title: Faker::Company.type)
+end
+
+15.times do
+	Gossip.create(
+		title: Faker::Movies::Hobbit.location,
+		content: Faker::Movies::Hobbit.quote,
+		user: User.all.sample)
+end
