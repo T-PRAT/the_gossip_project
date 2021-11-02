900a8d0bdea3821de5fb308ae35609362ff2421b
tprat <Titouan.p@hotmail.fr>
2021-11-02 02:31:32 +0100

    team contact

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get '/', to: 'static#index'
+  get 'team', to: 'static#team'
+  get 'contact', to: 'static#contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
