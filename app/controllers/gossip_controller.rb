ccb7ed2e831fadc3e2a53ad1bf4593466b97e0e9
tprat <Titouan.p@hotmail.fr>
2021-11-04 03:18:40 +0100

    gossip ok sauf new

--- app/controllers/gossip_controller.rb
+++ app/controllers/gossip_controller.rb
@@ -1,5 +1,40 @@
 class GossipController < ApplicationController
+  def index
+    @gossips = Gossip.all
+  end
+
   def show
-    puts @gossip = Gossip.find(params[:id])
+    @gossip = Gossip.find(params[:id])
+  end
+
+  def edit
+    @gossip = Gossip.find(params[:id])
+  end
+
+  def update
+    @gossip = Gossip.find(params[:id])
+    @gossip.update(post_params)
+    redirect_to gossip_path
+  end
+
+  def create
+    Gossip.create(post_params)
+    redirect_to gossip_index_path
+  end
+
+  def new
+    @gossip = Gossip.new
+  end
+
+  def destroy
+    @gossip = Gossip.find(params[:id])
+    @gossip.destroy
+    redirect_to gossip_index_path
+  end
+
+  private
+
+  def post_params
+    post_params = params.require(:gossip).permit(:title, :content)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
-  get 'gossip/show'
+  root to: 'gossip#index'
   get 'user/new'
   get 'user/info'
   get '/', to: 'static#index'
   get 'team', to: 'static#team'
   get 'contact', to: 'static#contact'
   get 'user/:first_name', to: 'user#show', as: 'user'
-  get 'gossip/:id', to: 'gossip#show'
 
-  #resources :users
+  resources :gossip
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
